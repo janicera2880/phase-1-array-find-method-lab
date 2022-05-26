e8ecb1a6f58c3bfbcd8d9f2683584594d915d71d
Janice Alecha <janiceralecha@gmail.com>
2022-05-25 20:43:16 -0500

    commit tree ad9f95f593acae8a5fbb822d955e24b00ae92734

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    const recordResult = record.find(recordYear=> recordYear.result === 'W')
+    if (recordResult === undefined) {
+
+    }
+
+else {
+    return recordResult.year;
+    }
+}
